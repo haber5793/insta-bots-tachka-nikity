76fa5c0fc0c0826952cf1a623a1167a6f8ce91dd
UNITAZ <romaminekraft2@gmail.com>
2025-07-16 18:50:13 +0300

    hs

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const clickBlock = document.querySelector(".voprosBLOK1")
 const hideBlock = document.querySelector(".otvetblock")
+let state = true
 
 
 clickBlock.addEventListener("click",()=>{
-    console.log(clickBlock)
-    hideBlock.classList.toggle('hidede')
-    clickBlock.classList.toggle('microanim')
-    
-    setTimeout(()=>{
-        hideBlock.classList.toggle('hide')
-    },500)
-})
+    if (state == true){
+        console.log('понос бобра')
+        setTimeout(() => {
+            hideBlock.classList.add('hidede')
+        }, 200);
+        state = false
 
+    }else{
+        hideBlock.classList.remove('hide')
+        console.log('еще понос бобра')
+        state =true
+        hideBlock.classList.remove('hidede')
+        setTimeout( () => {
+            hideBlock.classList.remove('hideanim')
+        }, 200
+    );
+    }
+})
