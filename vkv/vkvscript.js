f804edd199bf6e8945726f0f789d64261e6e31f7
UNTITAZ <ivanmorozov22820@gmail.com>
2025-07-16 17:46:07 +0300

    a

--- vkv/vkvscript.js
+++ vkv/vkvscript.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+const input1 = document.getElementById('num1')
+const button = document.getElementById('but')
+const input2 = document.getElementById('num2')
+
+button.addEventListener("click",()=>{
+    let num1 = Number(input1.value)
+    let num2 = Number(input2.value)
+    let c = Math.sqrt(num1*num1+num2*num2)
+    console.log(c)
+})
