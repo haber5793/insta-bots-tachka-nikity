056a2df5cfa3e95ac2ce0f5f977d57b31ca897f0
NikitaKikita007 <119515103+NikitaKikita007@users.noreply.github.com>
2025-07-16 18:06:02 +0300

    ssss

---
+++ sss.js
@@ -0,0 +1,8 @@
+const input1 = document.getElementById('num1')
+const button = document.getElementById('but')
+
+button.addEventListener("click",()=>{
+    let num1 = Number(input1.value)
+    let c = Math.sqrt(a*a+b*b)
+    console.log(c)
+})
